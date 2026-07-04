658fcb56cafa3e89e3f11c0820974479f4565596
Aryan <katocharyan743@gmail.com>
2026-07-04 23:45:14 +0530

    Day 1 of mega project-1

--- MEGA PROJECT 1 - JARVIS/main.py
+++ MEGA PROJECT 1 - JARVIS/main.py
@@ -1,1 +1,13 @@
 import speech_recognition as sr
+import pyttsx3
+import webbrowser
+
+r = sr.Recognizer()
+engine = pyttsx3.init()
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait
+
+if __name__ == "__main__":
+    speak("Initializing Jarvis")
