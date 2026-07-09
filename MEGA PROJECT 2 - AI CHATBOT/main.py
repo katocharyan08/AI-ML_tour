9973314ddfb7cefcc8f8de4037462b3242a4b4d3
Aryan <katocharyan743@gmail.com>
2026-07-09 17:06:39 +0530

    Little change

--- MEGA PROJECT 2 - AI CHATBOT/main.py
+++ MEGA PROJECT 2 - AI CHATBOT/main.py
@@ -8,29 +8,29 @@ from openai import OpenAI
 #     api_key = os.getenv("OPENAI_API_KEY")
 # )
 
-# Give yourself a few seconds to switch to the target window
-time.sleep(3)
 
-# Click at (1376, 1052)
+time.sleep(5)
+
+
 pyautogui.click(1376, 1052)
 time.sleep(0.2)
 while True:
-    # Drag to select the text
+  
     pyautogui.moveTo(677, 193)
     pyautogui.dragTo(774, 1008, duration=0.5, button='left')
 
     time.sleep(0.2)
 
-    # Copy selected text (Ctrl + C)
+   
     pyautogui.hotkey('ctrl', 'c')
 
-    # Wait for clipboard to update
+  
     time.sleep(0.2)
 
-    # Store clipboard text in a variable
+    
     copied_text = pyperclip.paste()
 
-    #For diselect
+   
     pyautogui.click(737, 288)
 
     print("Copied Text:")
@@ -50,11 +50,12 @@ while True:
     response = 'hanji'
     pyperclip.copy(response)
 
-    #click
+   
     pyautogui.click(988,978)
 
-    #paste
+  
     pyautogui.hotkey('ctrl','v')
 
-    #prss enter 
+     
     pyautogui.press('enter')
+    
